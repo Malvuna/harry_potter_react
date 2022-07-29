18839c97141dcd71a5ca7a3dac341c98f3e29eb9
Malvuna <3405635@gmail.com>
2022-07-29 16:33:31 +0300

    fix

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,8 +2,8 @@ export function Header() {
   return (
     <header>
       <div className="conteiner">
-        <h1 className="h1">Harry Potter</h1>
-        <h2 className="h2">
+        <h1 className="headh1">Harry Potter</h1>
+        <h2 className="headh2">
           View all characters from the Harry Potter universe
         </h2>
       </div>
